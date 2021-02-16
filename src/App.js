590da366c196c173350ac668748809c300ad720d
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-02-16 18:59:33 -0500

    Added Things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-import NavigationBar from './site/NavBar';
-import React, { useState, useEffect } from 'react';
-import LoggedIn from './site/Auth/LoggedIn';
-import BeforeLogIn from './site/Auth/BeforeLogin';
+import "./App.css";
+import NavigationBar from "./site/NavBar";
+import React, { useState, useEffect } from "react";
+import LoggedIn from "./site/Auth/LoggedIn";
+import BeforeLogIn from "./site/Auth/BeforeLogin";
+import Footer from "./components/Footer";
 
 function App() {
   // app.get('/*', function(req, res) {
@@ -13,40 +14,47 @@ function App() {
   //   })
   // })
 
-
-  const [sessionToken, setSessionToken] = useState('');
+  const [sessionToken, setSessionToken] = useState("");
   //using useState Hook to create a new state variable, sessionToken, will be empty, then have value, then emptied again
 
   useEffect(() => {
-    if (localStorage.getItem('token')){
-      setSessionToken(localStorage.getItem('token'));
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
     }
-  }, [])
+  }, []);
   //only runs on initial component load, because dependency is set to nothing. Just sets token to the localStorage token, if it exists
 
   const updateToken = (newToken) => {
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
     setSessionToken(newToken);
     console.log(sessionToken);
-  }
+  };
   //this takes a token (from API call) and sets it in both localStorage and in the variable.
 
   const clearToken = () => {
     localStorage.clear();
-    setSessionToken('');
-    console.log("Token cleared and Logged out")
-  }
+    setSessionToken("");
+    console.log("Token cleared and Logged out");
+  };
 
   const loggedInVsOut = () => {
-    return (sessionToken === localStorage.getItem('token') ? <LoggedIn token={sessionToken} /> : <BeforeLogIn updateToken={updateToken} /> )
-  }
-
+    return sessionToken === localStorage.getItem("token") ? (
+      <LoggedIn token={sessionToken} />
+    ) : (
+      <BeforeLogIn updateToken={updateToken} />
+    );
+  };
 
   return (
     <div>
-      <NavigationBar updateToken={updateToken} clearToken={clearToken} token={sessionToken}/>
-      
+      <NavigationBar
+        updateToken={updateToken}
+        clearToken={clearToken}
+        token={sessionToken}
+      />
+
       {loggedInVsOut()}
+      <Footer />
     </div>
   );
 }

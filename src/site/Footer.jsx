f927cc879610978e2a177037742035c1d6a24324
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-02-16 20:31:33 -0500

    Made Footer

--- src/site/Footer.jsx
+++ src/site/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-2 bg-dark fixed-bottom">
+      <footer id="footer" class="py-1 fixed-bottom">
         <div class="container">
-          <p class="m-0 text-center text-white">
+          <p class="m-0 text-center text-black">
             Copyright &copy; Project Beast Mode 2021
           </p>
         </div>
--- src/site/Header.jsx
+++ src/site/Header.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
 
-const Header = () => {
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div>
-      <p>Hello from Navbar</p>
+      <Navbar id="navBar" dark expand="md">
+        <NavbarBrand>Beast : Mode</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Register</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">Log In</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Nope</NavbarText>
+        </Collapse>
+      </Navbar>
     </div>
   );
 };

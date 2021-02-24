3cad738ac6c09349b384a0e7c38dc85ec3fa1e28
gingeralford <Ginger@Gingers-MacBook-Pro.local>
2021-02-23 21:17:47 -0500

    cleaned up the files some

--- src/site/Auth/LoggedIn.jsx
+++ src/site/Auth/LoggedIn.jsx
@@ -1,76 +1,15 @@
 import React from "react";
-import EventInfo from "../Events/EventInfo";
-import MessagesIndex from "../messages/MessagesIndex"
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-// import {container, row, col} from 'bootstrap'
-import {Container, Row, Col} from 'reactstrap'
-import DisplayMessages from "../MessageBoard/DisplayMessages";
 import BeforeLogIn from "./BeforeLogin";
 import Dashboard from "./Dashboard";
-// import r from '../../../node_modules/react-big-calendar/lib/css';
 
 
-// const localizer = momentLocalizer(moment);
-
-// //test array of events for now to try out calendar
-// let events = [ {
-//   title: "Big Mommas Day",
-//   start: 20210220,
-//   end: 20210220,
-//   allDay: false,
-//   resource: "blah",
-// },
-// {
-//   title: "Birthday",
-//   start: 20210225,
-//   end: 20210225,
-//   allDay: true,
-//   resource: "blah",
-// }
-// ]
 
 
 
 const LoggedIn = (props) => {
   return (
 
-    // <div id="loggedInPage">
-    //   <div id="heroIntro">
-    //     <p className="intro">Prepare.Yourself</p>
-    //   </div>
-    //   <p className="placeholder"></p>
-
-   
-    //   <Container>
-        
-    //     <Row>
-
-    //       <Col md="6">
-    //         <div className="columnHeader" style={{ width: "90%"}}>
-    //           <h2>Calendar</h2>
-    //         </div>
-    //         <EventCalendar />
-    //         <div className="columnHeader" style={{ width: "90%", "margin-top": "40px"}}><h2>Upcoming Events</h2></div>
-    //         <div id="displayEvents">
-    //           <EventInfo token={props.token} />
-    //         </div>
-    //       </Col>
-          
-    //       <Col md="6">
-    //         <div className="columnHeader" style={{width: "90%"}}>
-    //           <h2>Message Board</h2>
-    //         </div>
-    //         <div id="displayMessages">
-    //           <MessagesIndex token={props.token}/>
-    //           <DisplayMessages />
-    //         </div>
-    //       </Col>
-          
-    //     </Row>
-    //   </Container>
-    //   </div>
       <div>
     {props.token === localStorage.getItem("token") ? 
     
@@ -78,24 +17,9 @@ const LoggedIn = (props) => {
     : 
       <BeforeLogIn updateToken={props.updateToken} loggedInVsOut={props.loggedInVsOut}/>}
       </div>
-//     <div>
-//       <p className="placeholder"></p>
-//       <EventInfo token={props.token} />
 
-//     </div>
   );
 };
 
 export default LoggedIn;
 
-// const EventCalendar = props => (
-//   <div id="calendar">
-//     <Calendar
-//       localizer={localizer}
-//       events={events}
-//       startAccessor="start"
-//       endAccessor="end"
-//       style={{ height: 320, width: 320, }}
-//     />
-//   </div>
-// )
